fb47ce1fcbdb7c905ec05d3f9b081fbb2e68a7a1
breaddo <kylasim@gmail.com>
2025-09-30 12:22:58 +0800

    list view skeleton

--- todo/src/app/pages/dashboard/components/task-card/task-card.ts
+++ todo/src/app/pages/dashboard/components/task-card/task-card.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-import { ExpandList } from '../expand-list/expand-list';
+import { ExpandList } from '../../../../common-components/expand-list/expand-list';
 import { Button } from '../../../../common-components/button/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-card',
-  imports: [ MatCardModule, ExpandList, Button ],
+  imports: [ MatCardModule, ExpandList, Button, RouterLink ],
   templateUrl: './task-card.html',
   styleUrl: './task-card.scss'
 })
--- todo/src/app/pages/list-view/list-view.ts
+++ todo/src/app/pages/list-view/list-view.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import { ExpandList } from '../../common-components/expand-list/expand-list';
+import { NgFor, NgIf } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule } from '@angular/forms';  
 
 @Component({
   selector: 'app-list-view',
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatIconModule, ExpandList, MatSelectModule, NgFor, NgIf, MatChipsModule, FormsModule],
   templateUrl: './list-view.html',
   styleUrl: './list-view.scss'
 })
 export class ListView {
+  tags: string[] = ['Angular', 'React', 'Vue', 'Svelte'];
+  selectedTags: string[] = [];
 
+  toggleTag(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index === -1) {
+      this.selectedTags.push(tag);
+    } else {
+      this.selectedTags.splice(index, 1);
+    }
+  }
 }
